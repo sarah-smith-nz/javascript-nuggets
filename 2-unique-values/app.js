84471b16270305e12402e6b968e5f6c8f41d15f2
Sarah Smith <s.smith.nz@gmail.com>
2021-04-03 15:29:46 +1300

    notes from turorial

--- 2-unique-values/app.js
+++ 2-unique-values/app.js
@@ -37,3 +37,23 @@ result.innerHTML = categories
     return `<button>${category}</button>`;
   })
   .join('');
+
+  //name of array is menu
+  //in object is two properties - name, category
+  //get only the unique categories 
+  //iterate over the categorie retunring only the wanted ones
+  //item is parameter. 
+  //use set (ES6) only returns unique values.
+  // new set pass in new array
+  // const categories = new Set(menu.map((item) => item.category))
+  //console.log(categories) //returns object.
+  //Turn back into array. 
+  //categories spread out set. [...] spread operator. put code into array
+  //add category of all returns ['all', 'breakfast' 'lunch' 'dinner']
+
+  //looking at html, already have div of result under unqiue values
+
+
+  //return template string map over and return buttons on each category.
+  //turn into string using join(')')
+
