adc219229637fe06d427237d7497bb42a82bcab3
Sarah Smith <s.smith.nz@gmail.com>
2021-04-03 16:15:20 +1300

    follow along on video

--- 3-dynamic-object-keys/app.js
+++ 3-dynamic-object-keys/app.js
@@ -1,3 +1,4 @@
+
 const person = ['hello', 'great'];
 
 const testPerson = person.map(function (item) {
@@ -11,3 +12,54 @@ const newPerson = person.map(function (item) {
   return item;
 });
 console.log(newPerson);
+
+//set up keys dynamically using square bracket notations
+
+let appState = 'loading'
+appState = 'error' //would change log to {error: true}
+const keyName = 'computer' //setting variable to whatever value the key is
+const app= {
+  //set up key
+  [appState]: true //console.log(app) //prints {loading: true}
+}
+app [keyName] = 'apple' //create new key equal to an apple
+//create variable
+
+const state = {
+  loading: true,
+  name: '', //empty string
+  job: ''
+}
+//add or change or update the values on the object
+//functon takes two arguments, key name and value
+function updateState(key,value){
+  state[key] = value
+}
+//update the key name 
+updateState('name', 'john')
+console.log(state) //{loading:true, name,'john', job: ''}
+
+//can create new keys too
+updateState('product', [])
+
+
+
+/* 
+dot notation
+const person = {
+  name: 'john' ,
+}
+console.log(person.name) //john
+person.age = 25
+console.log(person) //{name:'john', age: 25}
+*/
+
+/*
+square bracket notation
+const items = {
+  'featured-items' : ['item1', 'item2']
+}
+trying to access this wont work
+console.log(items['featured-items']) //prints ['item1', 'item2']
+
+*/
